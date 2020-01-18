d63e4b23771a51b8b9864c959f1df64c6734d045
Paul Pan <panjunhong05@gmail.com>
2020-01-18 09:10:34 -0800

    Project milestone

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='static')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ run_flask.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=index.py
+export FLASK_ENV=development
+flask run
